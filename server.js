660b9a8b9796641e7ae8ec0ad0bd7e8c16e47772
monusk111 <mskakade111@gmail.com>
2023-02-27 05:22:04 +0530

    added cors support

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./config/db.js";
 
 import routes from "./routes/routes.js";
@@ -8,6 +9,7 @@ const app = express();
 const PORT = 3050;
 dotenv.config();
 connectDB();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", routes);
